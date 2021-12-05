efb70d18e7f849242b912d6ce0b083a03d9ac3f4
Hans van Dodewaard <hans@s104.nl>
2021-12-05 12:05:50 +0100

    day 5, part 1

--- src/main/kotlin/Advent5.kt
+++ src/main/kotlin/Advent5.kt
@@ -10,11 +10,47 @@ fun main() {
 }
 
 object advent5 {
-    fun advent5a(lines: Sequence<String>): Int {
-        return 0
+    data class Line(val x0:Int, val y0:Int, val x1:Int, val y1:Int) {
+        val isHorizontal = y0 == y1
+        val isVertical = x0 == x1
+        val xMin = if (x0 < x1) x0 else x1
+        val xMax = if (x0 > x1) x0 else x1
+        val yMin = if (y0 < y1) y0 else y1
+        val yMax = if (y0 > y1) y0 else y1
+    }
+    class Matrix(lines:List<Line>) {
+        val w = lines.maxOf { maxOf(it.x0,it.x1) } + 1
+        val h = lines.maxOf {  maxOf(it.y0, it.y1) } + 1
+        val content = Array(h) { Array(w) { 0 } }
+        init {
+            lines.forEach { addLine(it) }
+        }
+
+        fun addLine(l:Line) {
+            require(l.x1 < w && l.y1 < h)
+            if (l.isHorizontal) for (i in l.xMin .. l.xMax) { content[l.y0][i] ++ }
+            else if (l.isVertical) for (i in l.yMin .. l.yMax) { content[i][l.x0] ++ }
+        }
+
+        fun countAtLeast(i:Int):Int {
+            return content.sumOf { r->r.map { if (it >= i) 1 else 0 }.sum() }
+        }
+    }
+    fun advent5a(input: Sequence<String>): Int {
+        val lines = input.mapNotNull { parseLine(it) }
+        val matrix = Matrix(lines.toList())
+        return matrix.countAtLeast(2)
     }
 
     fun advent5b(lines: Sequence<String>): Int {
         return 0
+    }
+
+
+    fun parseLine(s:String):Line? {
+        val reg = """\s*(\d+),(\d+)\s*->\s*(\d+),(\d+)""".toRegex()
+        val match = reg.matchEntire(s) ?: return null
+        return Line(match.groupValues[1].toInt(), match.groupValues[2].toInt(),
+                    match.groupValues[3].toInt(), match.groupValues[4].toInt())
     }
 }
--- src/test/kotlin/Advent5Test.kt
+++ src/test/kotlin/Advent5Test.kt
@@ -1,26 +1,46 @@
 import advent5.advent5a
 import advent5.advent5b
+import advent5.parseLine
 import org.junit.jupiter.api.Test
 import java.io.File
 import kotlin.test.assertEquals
 
 class Advent5Test {
 
-    val testInput = """"""
+    val testInput = """0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2"""
 
     @Test()
     fun advent5aBaseTest() {
-        assertEquals(0, advent5a("00000".lineSequence()))
+        assertEquals(advent5.Line(0, 9, 5, 9), parseLine("0,9 -> 5,9"))
+        assertEquals(0, advent5a(testInput.lineSequence().take(1)))
+        assertEquals(0, advent5a(testInput.lineSequence().take(2)))
+        assertEquals(6, advent5a("0,9->5,9\n0,9->5,9".lineSequence()))
+        assertEquals(5, advent5a("0,9->5,9\n1,9->6,9".lineSequence()))
+        assertEquals(6, advent5a("9,0->9,5\n9,0->9,5".lineSequence()))
+        assertEquals(5, advent5a("9,0->9,5\n9,1->9,6".lineSequence()))
+        assertEquals(6, advent5a("9,5->9,0\n9,5->9,0".lineSequence()))
+        assertEquals(1, advent5a("5,0->5,9\n0,5->9,5".lineSequence()))
+        assertEquals(19, advent5a("5,0->5,9\n0,5->9,5\n5,0->5,9\n0,5->9,5".lineSequence()))
+        assertEquals(1, advent5a("5,9->5,0\n9,5->0,5".lineSequence()))
     }
 
     @Test()
     fun advent5aInputTest() {
-        assertEquals(0, advent5a(testInput.lineSequence()))
+        assertEquals(5, advent5a(testInput.lineSequence()))
     }
 
     @Test
     fun advent5aTest() {
-        assertEquals(0, File("input/input5.txt").useLines { advent5a(it) })
+        assertEquals(7644, File("input/input5.txt").useLines { advent5a(it) })
     }
 
     @Test()
