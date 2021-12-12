71b75ccce3dbfc3b8f2c88b0846dbacdc2608922
Hans van Dodewaard <hans@s104.nl>
2021-12-12 13:36:08 +0100

    day 11, part 1

--- src/main/kotlin/Day12.kt
+++ src/main/kotlin/Day12.kt
@@ -1,6 +1,7 @@
-import Day12.part1
-import Day12.part2
+import Day12.Companion.part1
+import Day12.Companion.part2
 import java.io.File
+import java.util.*
 
 fun main() {
     File("input/input12.txt").useLines {
@@ -9,12 +10,78 @@ fun main() {
     }
 }
 
-object Day12 {
-    fun part1(lines: Sequence<String>): Int {
-        return 0
+class Cave(val name:String) {
+    val isBig = name == name.uppercase(Locale.getDefault())
+    val connections = mutableSetOf<String>()
+
+    fun addConnection(name:String) {
+        connections.add(name)
     }
 
-    fun part2(lines: Sequence<String>): Int {
-        return 0
+    override fun toString():String = name
+}
+
+typealias Route = List<Cave>
+fun Route.isValid(part2:Boolean):Boolean {
+    for (c in this) {
+        if (!c.isBig && (this.count { it.name == c.name } > 1)) return false
     }
+    return true
+}
+
+class Day12(val part2:Boolean = false) {
+
+
+    private val caves = hashMapOf<String,Cave>()
+    init {
+        getOrCreate("start")
+        getOrCreate("end")
+    }
+
+    fun getOrCreate(name:String):Cave = caves[name] ?: let {
+        val n = Cave(name)
+        caves[name] = n
+        n
+    }
+
+    fun parseInput(line:String) {
+        val names = line.split('-')
+        if (names.size != 2) return
+        val c1 = getOrCreate(names[0])
+        val c2 = getOrCreate(names[1])
+        c1.addConnection(c2.name)
+        c2.addConnection(c1.name)
+    }
+
+
+    fun findRoutes(s: String, e: String): List<Route> {
+        val start = caves[s] ?: return listOf()
+        val end = caves[e] ?: return listOf()
+        return findRoutes(listOf(),start, end)
+    }
+    private fun findRoutes(routeBase:Route, next:Cave, end: Cave): List<Route> {
+
+        if (next.name == end.name) return listOf(routeBase+end)
+        val route = routeBase+next
+        if (!(route).isValid(part2)) return listOf()
+        return next.connections.mapNotNull { caves[it] }
+            .fold(listOf()) { result, connection -> result + findRoutes(route, connection, end) }
+    }
+
+    companion object {
+        fun part1(lines: Sequence<String>): Int {
+            val day12 = Day12()
+            lines.forEach { day12.parseInput(it) }
+            val routes = day12.findRoutes("start","end")
+            return routes.size
+        }
+
+        fun part2(lines: Sequence<String>): Int {
+            val day12 = Day12(true)
+            lines.forEach { day12.parseInput(it) }
+            val routes = day12.findRoutes("start","end")
+            return routes.size
+        }
+    }
+
 }
