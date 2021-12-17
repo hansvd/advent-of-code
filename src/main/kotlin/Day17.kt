431d195851c6365cc4c8324e2dac5df0d91105e0
Hans van Dodewaard <hans@s104.nl>
2021-12-17 11:37:32 +0100

    day 17, part 2 refactor

--- src/main/kotlin/Day17.kt
+++ src/main/kotlin/Day17.kt
@@ -1,34 +1,32 @@
 
-
+typealias Path = List<Day17.Point>
 object Day17 {
     fun go(x:IntRange, y:IntRange, v:Velocity): Point? = Trench(x,y).go(Point(0,0),v).lastOrNull()
 
     fun part1(x:IntRange, y:IntRange): Int
-        = Trench(x,y).findVelocity().maxOf { r -> r.second.maxOf { it.y }  }
+        = Trench(x,y).findVelocities().maxOf { r -> r.second.maxOf { it.y }  }
 
-    fun part2(x:IntRange, y:IntRange): Int = Trench(x,y).findVelocity().count()
+    fun part2(x:IntRange, y:IntRange): Int = Trench(x,y).findVelocities().count()
 
     data class Point(val x:Int, val y:Int) {
-        fun move(v:Velocity):Point = Point(x + v.x,y+v.y)
+        fun move(v:Velocity):Point = Point(x + v.dx,y+v.dy)
     }
-    data class Velocity(val x:Int, val y:Int) {
-        fun next():Velocity = Velocity(if (x > 0) x-1 else if (x < 0) x+1 else 0, y-1)
+    data class Velocity(val dx:Int, val dy:Int) {
+        fun next():Velocity = Velocity(if (dx > 0) dx-1 else if (dx < 0) dx+1 else 0, dy-1)
     }
     class Trench(private val targetX:IntRange, private val targetY:IntRange) {
 
-        fun findVelocity():Sequence<Pair<Velocity,List<Point>>> {
-            return sequence {
-                for (x in 1..targetX.last) {
-                    for (y in targetY.first..-targetY.first) {
-                        val s = go(Point(0, 0), Velocity(x, y))
-                        if (s.isNotEmpty()) yield(Pair(Velocity(x, y),s))
-                    }
+        fun findVelocities():Sequence<Pair<Velocity,Path>> = sequence {
+            (1..targetX.last).forEach { x->
+                (targetY.first..-targetY.first).forEach { y->
+                    val s = go(Point(0, 0), Velocity(x, y))
+                    if (s.isNotEmpty()) yield(Pair(Velocity(x, y),s))
                 }
             }
         }
-        fun go (start:Point = Point(0,0), velocity: Velocity) :List<Point>
+        fun go (start:Point = Point(0,0), velocity: Velocity) :Path
             = go (listOf(start),velocity)
-        private fun go(cur:List<Point>, velocity:Velocity):List<Point> {
+        private fun go(cur:Path, velocity:Velocity):Path {
             val last = cur.last()
             if (hit(last)) return cur
             if (toFar(last)) return listOf()
