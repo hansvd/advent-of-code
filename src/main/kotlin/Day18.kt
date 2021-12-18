097ea8e98d319b1f569b639d426def991f6be3c9
Hans van Dodewaard <hans@s104.nl>
2021-12-18 21:40:32 +0100

    day 18, part 1 with problems

--- src/main/kotlin/Day18.kt
+++ src/main/kotlin/Day18.kt
@@ -1,55 +1,76 @@
-
-
 object Day18 {
-    data class ReduceResult(val v:Value, val addLeft:Int=0, val addRight:Int=0, val done:Boolean=false)
+    data class ReduceResult(val v: Value, val addLeft: Int = 0, val addRight: Int = 0, val done: Boolean = false)
 
     interface Value {
         //val depth:In
-        fun explode(depth:Int):ReduceResult
-        fun split():ReduceResult
-        fun explode():Value {
+        fun explode(depth: Int): ReduceResult
+        fun split(): ReduceResult
+        fun explode(): Value {
             return explode(0).v
         }
-        fun splitter():Value {
+
+        fun splitter(): Value {
             return split().v
         }
-        fun reduce():Value {
+
+        fun reduce(): Value {
             var result = this
+            println(result.toString())
             do {
                 val explodeResult = result.explode(0)
-                val splitResult = explodeResult.v.split()
+                if (explodeResult.done) println("explode." + explodeResult.v.toString(0))
+                var splitResult = explodeResult.v.split()
+                if (splitResult.done) println("split..." + splitResult.v.toString(0))
+//                while (splitResult.done) {
+//                    splitResult = splitResult.v.split()
+//                    if (splitResult.done) println(splitResult.v.toString())
+//                }
+
                 result = splitResult.v
-            } while(explodeResult.done || splitResult.done)
+
+            } while (explodeResult.done || splitResult.done)
+            println("==")
             return result
         }
-        fun addFirst(value:Int):ReduceResult
+
+        fun addFirst(value: Int): ReduceResult
+        fun addLast(value: Int): ReduceResult
+        fun toString(depth:Int):String
     }
-    data class NumValue(val num:Int):Value {
-        override fun explode(depth:Int):ReduceResult {
+
+    data class NumValue(val num: Int) : Value {
+        override fun explode(depth: Int): ReduceResult {
             //if (num >= 10) return this
             return ReduceResult(this)
         }
 
         override fun split(): ReduceResult {
             if (num < 10) return ReduceResult(this)
-            return ReduceResult(SnailFishValue((num / 2).toValue(),((num + 1) / 2).toValue()),done=true)
+            return ReduceResult(SnailFishValue((num / 2).toValue(), ((num + 1) / 2).toValue()), done = true)
         }
 
         override fun addFirst(value: Int): ReduceResult {
-            return ReduceResult(NumValue(num + value),done=true)
+            return ReduceResult(NumValue(num + value), done = true)
         }
+
+        override fun addLast(value: Int): ReduceResult {
+            return ReduceResult(NumValue(num + value), done = true)
+        }
+
+        override fun toString(depth: Int) = num.toString()
 
         override fun toString(): String = num.toString()
     }
-    data class SnailFishValue(val left:Value, val right:Value):Value {
+
+    data class SnailFishValue(val left: Value, val right: Value) : Value {
         //override val depth = 1 + maxOf(left.depth,right.depth)
 
-        override fun explode(depth:Int):ReduceResult {
+        override fun explode(depth: Int): ReduceResult {
+//            assert(depth < 3 || (left is NumValue && right is NumValue))
             if (depth == 4 && (left is NumValue && right is NumValue)) {
-                return ReduceResult(0.toValue(),left.num,right.num,true)
-            }
-            else {
-                val leftResult = left.explode(depth+1)
+                return ReduceResult(0.toValue(), left.num, right.num, true)
+            } else {
+                val leftResult = left.explode(depth + 1)
                 var addLeft = leftResult.addLeft
                 var addRight = leftResult.addRight
 
@@ -57,7 +78,9 @@ object Day18 {
                     val r = right.addFirst(addRight)
                     addRight = 0
                     r
-                } else right.explode(depth+1)
+                } else {
+                    right.explode(depth + 1)
+                }
 
 
 
@@ -65,15 +88,28 @@ object Day18 {
                 addRight = addRight + rightResult.addRight
                 var newLeft = leftResult.v
                 var newRight = rightResult.v
-                if (newLeft is NumValue && addLeft > 0 && (!leftResult.done || depth < 3)) {
+                if (newLeft is NumValue && addLeft > 0 && (!leftResult.done || depth < depth - 1)) {
                     newLeft = NumValue(newLeft.num + addLeft)
                     addLeft = 0
                 }
-                if (newRight is NumValue && addRight > 0 && (!rightResult.done || depth < 3)) {
+                if (newRight is NumValue && addRight > 0 && (!rightResult.done || depth < depth - 1)) {
                     newRight = NumValue(newRight.num + addRight)
                     addRight = 0
                 }
-                return ReduceResult(SnailFishValue(newLeft,newRight),addLeft,addRight,leftResult.done || rightResult.done)
+                if (rightResult.done && addLeft > 0 && !leftResult.done) {
+                    val addLeftResult = newLeft.addLast(addLeft)
+                    if (addLeftResult.done) {
+                        newLeft = addLeftResult.v
+                        addLeft = 0
+                    }
+
+                }
+                return ReduceResult(
+                    SnailFishValue(newLeft, newRight),
+                    addLeft,
+                    addRight,
+                    leftResult.done || rightResult.done
+                )
             }
 
         }
@@ -81,24 +117,35 @@ object Day18 {
         override fun split(): ReduceResult {
             val leftResult = left.split()
             if (leftResult.done) {
-                return ReduceResult(SnailFishValue(leftResult.v,right), done = true)
+                return ReduceResult(SnailFishValue(leftResult.v, right), done = true)
             }
             val rightResult = right.split()
-            return ReduceResult(SnailFishValue(left,rightResult.v),done=rightResult.done)
+            return ReduceResult(SnailFishValue(left, rightResult.v), done = rightResult.done)
         }
 
         override fun addFirst(value: Int): ReduceResult {
-             val leftResult = left.addFirst(value)
+            val leftResult = left.addFirst(value)
             if (leftResult.done)
-                return ReduceResult(SnailFishValue(leftResult.v,right),done=true)
+                return ReduceResult(SnailFishValue(leftResult.v, right), done = true)
 
             val rightResult = right.addFirst(value)
-            return ReduceResult(SnailFishValue(left,rightResult.v),done = rightResult.done)
+            return ReduceResult(SnailFishValue(left, rightResult.v), done = rightResult.done)
+        }
 
+        override fun addLast(value: Int): ReduceResult {
+            val result = right.addLast(value)
+            return ReduceResult(SnailFishValue(left, result.v), done = result.done)
+
+        }
+
+        override fun toString(depth: Int): String {
+             val s = if (depth == 4) '|' else '['
+            return "$s${left.toString(depth+1)},${right.toString(depth+1)}]"
         }
 
         override fun toString(): String = "[$left,$right]"
     }
+
     fun Int.toValue() = NumValue(this)
     fun part1(lines: Sequence<String>): Int {
         return 0
@@ -108,26 +155,31 @@ object Day18 {
         return 0
     }
 
-    fun parseInput(line:String):SnailFishValue {
+    fun parseInputLines(input: Sequence<String>): Value =
+        input.map { parseInput(it) }.reduce { r, l -> SnailFishValue(r, l).reduce() }
+
+    fun parseInput(line: String): Value {
         require(line.isNotBlank() && line.first() == '[' && line.last() == ']')
 
         val separatorIndex = pairSeparatorIndex(line)
-        val left = parseValue(line.substring(1,separatorIndex))
-        val right = parseValue(line.substring(separatorIndex+1,line.length-1))
-        return SnailFishValue(left,right)
+        val left = parseValue(line.substring(1, separatorIndex))
+        val right = parseValue(line.substring(separatorIndex + 1, line.length - 1))
+        return SnailFishValue(left, right)
     }
-    fun pairSeparatorIndex(line:String):Int {
+
+    fun pairSeparatorIndex(line: String): Int {
         var numPairs = 0
-        for(i in line.indices) {
+        for (i in line.indices) {
             if (line[i] == '[') numPairs++
             if (line[i] == ']') numPairs--
             if (numPairs <= 1 && line[i] == ',') return i
         }
         return line.length
     }
-    fun parseValue(line:String):Value {
-        var i = 0; while(i < line.length && line[i].isDigit()) i++
-        if (i>0) return NumValue(Integer.parseInt(line.substring(0,i)))
+
+    fun parseValue(line: String): Value {
+        var i = 0; while (i < line.length && line[i].isDigit()) i++
+        if (i > 0) return NumValue(Integer.parseInt(line.substring(0, i)))
         if (line[0].isDigit()) return NumValue(line[0].code - '0'.code)
         return parseInput(line)
     }
--- src/test/kotlin/Day18Test.kt
+++ src/test/kotlin/Day18Test.kt
@@ -1,5 +1,6 @@
 import Day18.SnailFishValue
 import Day18.parseInput
+import Day18.parseInputLines
 import Day18.parseValue
 import Day18.toValue
 import DayX.part1
@@ -11,30 +12,63 @@ import kotlin.test.assertNotNull
 
 class Day18Test {
 
-    val testInput = """[1,2]
-[[1,2],3]
-[9,[8,7]]
-[[1,9],[8,5]]
-[[[[1,2],[3,4]],[[5,6],[7,8]]],9]
-[[[9,[3,8]],[[0,9],6]],[[[3,7],[4,9]],3]]
-[[[[1,3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]]"""
+    val testInput = """[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]
+[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]
+[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]
+[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]
+[7,[5,[[3,8],[1,4]]]]
+[[2,[2,2]],[8,[8,1]]]
+[2,9]
+[1,[[[9,3],9],[[9,0],[0,7]]]]
+[[[5,[7,4]],7],1]
+[[[[4,2],2],6],[8,7]]"""
 
     @Test
     fun parseValueTest() {
         assertEquals(1.toValue(), parseValue("1"))
         assertEquals(SnailFishValue(1.toValue(), 2.toValue()), parseValue("[1,2]"))
-        assertEquals(SnailFishValue(SnailFishValue(1.toValue(), 2.toValue()),3.toValue()), parseInput("[[1,2],3]"))
+        assertEquals(SnailFishValue(SnailFishValue(1.toValue(), 2.toValue()), 3.toValue()), parseInput("[[1,2],3]"))
         assertNotNull(parseInput("[[[1,3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]]"))
     }
+
     @Test
-    fun exampleInputTestPart1() {
+    fun exampleInputTest() {
         assertEquals("[[[[0,9],2],3],4]", parseInput("[[[[[9,8],1],2],3],4]").explode().toString())
         assertEquals("[7,[6,[5,[7,0]]]]", parseInput("[7,[6,[5,[4,[3,2]]]]]").explode().toString())
         assertEquals("[[6,[5,[7,0]]],3]", parseInput("[[6,[5,[4,[3,2]]]],1]").explode().toString())
-        assertEquals("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]", parseInput("[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]").explode().toString())
-        assertEquals("[[3,[2,[8,0]]],[9,[5,[7,0]]]]", parseInput("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]").explode().toString())
-        assertEquals("[[[[0,7],4],[[7,8],[0,13]]],[1,1]]", parseInput("[[[[0,7],4],[15,[0,13]]],[1,1]]").splitter().toString())
-        assertEquals("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]", parseInput("[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]").reduce().toString())
+        assertEquals("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]",
+            parseInput("[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]").explode().toString()
+        )
+        assertEquals("[[3,[2,[8,0]]],[9,[5,[7,0]]]]",
+            parseInput("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]").explode().toString()
+        )
+        assertEquals("[[[[0,7],4],[[7,8],[0,13]]],[1,1]]",
+            parseInput("[[[[0,7],4],[15,[0,13]]],[1,1]]").splitter().toString()
+        )
+        assertEquals("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]",
+            parseInput("[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]").reduce().toString()
+        )
+    }
+
+    @Test fun inputListTest() {
+        assertEquals("[[[[1,1],[2,2]],[3,3]],[4,4]]", parseInputLines("[1,1]\n[2,2]\n[3,3]\n[4,4]".lineSequence()).toString())
+        assertEquals("[[[[3,0],[5,3]],[4,4]],[5,5]]", parseInputLines("[1,1]\n[2,2]\n[3,3]\n[4,4]\n[5,5]".lineSequence()).toString())
+        assertEquals("[[[[5,0],[7,4]],[5,5]],[6,6]]", parseInputLines("[1,1]\n[2,2]\n[3,3]\n[4,4]\n[5,5]\n[6,6]".lineSequence()).toString())
+
+        assertEquals("[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]", parseInput("[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]").reduce().toString())
+        assertEquals("[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]", parseInput("[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]").reduce().toString())
+
+
+    }
+    @Test
+    fun exampleInputListTest() {
+        assertEquals("[[[[6,6],[7,7]],[[0,7],[7,7]]],[[[5,5],[5,6]],9]]", parseInput("[[[[[6,6],[6,6]],[[6,0],[6,7]]],[[[7,7],[8,9]],[8,[8,1]]]],[2,9]]").reduce().toString())
+
+//        assertEquals("[[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]]", parseInput("[[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]],[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]]").reduce().toString())
+//        assertEquals("[[[[6,7],[6,7]],[[7,7],[0,7]]],[[[8,7],[7,7]],[[8,8],[8,0]]]]",parseInput("[[[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]],[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]]").reduce().toString())
+//        assertEquals("[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]", parseInput("[[[[[7,7],[7,7]],[[8,7],[8,7]]],[[[7,0],[7,7]],9]],[[[[4,2],2],6],[8,7]]]").reduce().toString())
+//        assertEquals("[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]", parseInputLines(testInput.lineSequence()).toString() )
+
     }
 
     @Test
