dac8acab52dc0a779c29314e88cb1a7423b2a9de
Hans van Dodewaard <hans@s104.nl>
2021-12-18 21:53:33 +0100

    day 18, part 1 reduce fixed

--- src/main/kotlin/Day18.kt
+++ src/main/kotlin/Day18.kt
@@ -1,10 +1,10 @@
 object Day18 {
-    data class ReduceResult(val v: Value, val addLeft: Int = 0, val addRight: Int = 0, val done: Boolean = false)
+    data class StepResult(val v: Value, val addLeft: Int = 0, val addRight: Int = 0, val done: Boolean = false)
 
     interface Value {
         //val depth:In
-        fun explode(depth: Int): ReduceResult
-        fun split(): ReduceResult
+        fun explode(depth: Int): StepResult
+        fun split(): StepResult
         fun explode(): Value {
             return explode(0).v
         }
@@ -17,44 +17,44 @@ object Day18 {
             var result = this
             println(result.toString())
             do {
-                val explodeResult = result.explode(0)
-                if (explodeResult.done) println("explode." + explodeResult.v.toString(0))
-                var splitResult = explodeResult.v.split()
-                if (splitResult.done) println("split..." + splitResult.v.toString(0))
-//                while (splitResult.done) {
-//                    splitResult = splitResult.v.split()
-//                    if (splitResult.done) println(splitResult.v.toString())
-//                }
+                var stepResult = result.explode(0)
+                if (stepResult.done) {
+                    println("explode." + stepResult.v.toString(0))
+                    result = stepResult.v
+                    continue
+                }
 
-                result = splitResult.v
+                stepResult = result.split()
+                if (stepResult.done) println("split..." + stepResult.v.toString(0))
+                result = stepResult.v
 
-            } while (explodeResult.done || splitResult.done)
+            } while (stepResult.done)
             println("==")
             return result
         }
 
-        fun addFirst(value: Int): ReduceResult
-        fun addLast(value: Int): ReduceResult
+        fun addFirst(value: Int): StepResult
+        fun addLast(value: Int): StepResult
         fun toString(depth:Int):String
     }
 
     data class NumValue(val num: Int) : Value {
-        override fun explode(depth: Int): ReduceResult {
+        override fun explode(depth: Int): StepResult {
             //if (num >= 10) return this
-            return ReduceResult(this)
+            return StepResult(this)
         }
 
-        override fun split(): ReduceResult {
-            if (num < 10) return ReduceResult(this)
-            return ReduceResult(SnailFishValue((num / 2).toValue(), ((num + 1) / 2).toValue()), done = true)
+        override fun split(): StepResult {
+            if (num < 10) return StepResult(this)
+            return StepResult(SnailFishValue((num / 2).toValue(), ((num + 1) / 2).toValue()), done = true)
         }
 
-        override fun addFirst(value: Int): ReduceResult {
-            return ReduceResult(NumValue(num + value), done = true)
+        override fun addFirst(value: Int): StepResult {
+            return StepResult(NumValue(num + value), done = true)
         }
 
-        override fun addLast(value: Int): ReduceResult {
-            return ReduceResult(NumValue(num + value), done = true)
+        override fun addLast(value: Int): StepResult {
+            return StepResult(NumValue(num + value), done = true)
         }
 
         override fun toString(depth: Int) = num.toString()
@@ -65,76 +65,73 @@ object Day18 {
     data class SnailFishValue(val left: Value, val right: Value) : Value {
         //override val depth = 1 + maxOf(left.depth,right.depth)
 
-        override fun explode(depth: Int): ReduceResult {
-//            assert(depth < 3 || (left is NumValue && right is NumValue))
+        override fun explode(depth: Int): StepResult {
+            require(depth != 4 || (left is NumValue && right is NumValue))
             if (depth == 4 && (left is NumValue && right is NumValue)) {
-                return ReduceResult(0.toValue(), left.num, right.num, true)
+                return StepResult(0.toValue(), left.num, right.num, true)
+            }
+            val leftResult = left.explode(depth + 1)
+            var addLeft = leftResult.addLeft
+            var addRight = leftResult.addRight
+
+            val rightResult = if (leftResult.done) {
+                val r = right.addFirst(addRight)
+                addRight = 0
+                r
             } else {
-                val leftResult = left.explode(depth + 1)
-                var addLeft = leftResult.addLeft
-                var addRight = leftResult.addRight
+                right.explode(depth + 1)
+            }
 
-                val rightResult = if (leftResult.done) {
-                    val r = right.addFirst(addRight)
-                    addRight = 0
-                    r
-                } else {
-                    right.explode(depth + 1)
-                }
-
-
-
-                addLeft = addLeft + rightResult.addLeft
-                addRight = addRight + rightResult.addRight
-                var newLeft = leftResult.v
-                var newRight = rightResult.v
-                if (newLeft is NumValue && addLeft > 0 && (!leftResult.done || depth < depth - 1)) {
-                    newLeft = NumValue(newLeft.num + addLeft)
+            addLeft += rightResult.addLeft
+            addRight += rightResult.addRight
+            var newLeft = leftResult.v
+            var newRight = rightResult.v
+            if (newLeft is NumValue && addLeft > 0 && (!leftResult.done || depth < depth - 1)) {
+                newLeft = NumValue(newLeft.num + addLeft)
+                addLeft = 0
+            }
+            if (newRight is NumValue && addRight > 0 && (!rightResult.done || depth < depth - 1)) {
+                newRight = NumValue(newRight.num + addRight)
+                addRight = 0
+            }
+            if (rightResult.done && addLeft > 0 && !leftResult.done) {
+                val addLeftResult = newLeft.addLast(addLeft)
+                if (addLeftResult.done) {
+                    newLeft = addLeftResult.v
                     addLeft = 0
                 }
-                if (newRight is NumValue && addRight > 0 && (!rightResult.done || depth < depth - 1)) {
-                    newRight = NumValue(newRight.num + addRight)
-                    addRight = 0
-                }
-                if (rightResult.done && addLeft > 0 && !leftResult.done) {
-                    val addLeftResult = newLeft.addLast(addLeft)
-                    if (addLeftResult.done) {
-                        newLeft = addLeftResult.v
-                        addLeft = 0
-                    }
 
-                }
-                return ReduceResult(
-                    SnailFishValue(newLeft, newRight),
-                    addLeft,
-                    addRight,
-                    leftResult.done || rightResult.done
-                )
             }
+            return StepResult(
+                SnailFishValue(newLeft, newRight),
+                addLeft,
+                addRight,
+                leftResult.done || rightResult.done
+            )
 
         }
 
-        override fun split(): ReduceResult {
+        override fun split(): StepResult {
             val leftResult = left.split()
             if (leftResult.done) {
-                return ReduceResult(SnailFishValue(leftResult.v, right), done = true)
+                return StepResult(SnailFishValue(leftResult.v, right), done = true)
             }
             val rightResult = right.split()
-            return ReduceResult(SnailFishValue(left, rightResult.v), done = rightResult.done)
+            return StepResult(SnailFishValue(left, rightResult.v), done = rightResult.done)
         }
 
-        override fun addFirst(value: Int): ReduceResult {
+        override fun addFirst(value: Int): StepResult {
             val leftResult = left.addFirst(value)
             if (leftResult.done)
-                return ReduceResult(SnailFishValue(leftResult.v, right), done = true)
+                return StepResult(SnailFishValue(leftResult.v, right), done = true)
 
             val rightResult = right.addFirst(value)
-            return ReduceResult(SnailFishValue(left, rightResult.v), done = rightResult.done)
+            return StepResult(SnailFishValue(left, rightResult.v), done = rightResult.done)
         }
 
-        override fun addLast(value: Int): ReduceResult {
+        override fun addLast(value: Int): StepResult {
             val result = right.addLast(value)
-            return ReduceResult(SnailFishValue(left, result.v), done = result.done)
+            return StepResult(SnailFishValue(left, result.v), done = result.done)
 
         }
 
@@ -147,13 +144,13 @@ object Day18 {
     }
 
     fun Int.toValue() = NumValue(this)
-    fun part1(lines: Sequence<String>): Int {
-        return 0
-    }
-
-    fun part2(lines: Sequence<String>): Int {
-        return 0
-    }
+//    fun part1(lines: Sequence<String>): Int {
+//        return 0
+//    }
+//
+//    fun part2(lines: Sequence<String>): Int {
+//        return 0
+//    }
 
     fun parseInputLines(input: Sequence<String>): Value =
         input.map { parseInput(it) }.reduce { r, l -> SnailFishValue(r, l).reduce() }
@@ -167,7 +164,7 @@ object Day18 {
         return SnailFishValue(left, right)
     }
 
-    fun pairSeparatorIndex(line: String): Int {
+    private fun pairSeparatorIndex(line: String): Int {
         var numPairs = 0
         for (i in line.indices) {
             if (line[i] == '[') numPairs++
