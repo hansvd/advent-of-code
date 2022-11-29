2b5de3efc9b84fddf48aa4b840dfa4612062f4ae
Hans van Dodewaard <hans@s104.nl>
2022-11-29 08:43:53 +0100

    prepare 2022

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.6.20"
+    kotlin("jvm") version "1.7.21"
     application
 }
 
@@ -21,7 +21,7 @@ tasks.test {
 }
 
 tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "14"
+    kotlinOptions.jvmTarget = "18"
 }
 
 application {
--- src/main/kotlin/shared/Input.kt
+++ src/main/kotlin/shared/Input.kt
@@ -4,7 +4,7 @@ import java.io.File
 
 object Input {
     fun <T> useLines(year: Int, name: String, block: (Sequence<String>) -> T): T =
-        File("input/aoc2021/aoc$year/$name.txt").useLines { block(it) }
+        File("input/aoc$year/$name.txt").useLines { block(it) }
 
     fun <T> useLines(year: Int, nb: Int, block: (Sequence<String>) -> T): T =
         File("input/aoc$year/$nb.txt").useLines { block(it) }
