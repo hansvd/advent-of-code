c6098e465bf7475b668809d7e05f04656456f995
Hans van Dodewaard <hans@s104.nl>
2022-12-01 10:20:48 +0100

    2022 - Day 1

--- src/main/kotlin/aoc2022/day01/Day202201.kt
+++ src/main/kotlin/aoc2022/day01/Day202201.kt
@@ -2,11 +2,19 @@ package aoc2022.day01
 
 object Day202201 {
 
-    fun part1(lines: Sequence<String>): Int {
-        return 0
+    private fun getSums(lines: List<String>): List<Int> {
+        val sums = mutableListOf<Int>()
+        sums.add(0)
+        lines.forEach { l ->
+            if (l.isBlank())
+                sums.add(0)
+            else
+                sums[sums.size - 1] += l.toInt()
+        }
+        return sums
     }
 
-    fun part2(lines: Sequence<String>): Int {
-        return 0
-    }
+    fun part1(lines: List<String>): Int = getSums(lines).max()
+
+    fun part2(lines: List<String>): Int = getSums(lines).sortedDescending().take(3).sum()
 }
--- src/main/kotlin/aoc2022/day01/Day202201Test.kt
+++ src/main/kotlin/aoc2022/day01/Day202201Test.kt
@@ -6,12 +6,35 @@ import kotlin.test.assertEquals
 
 class Day202201Test {
 
+    val input = "1000\n" +
+            "2000\n" +
+            "3000\n" +
+            "\n" +
+            "4000\n" +
+            "\n" +
+            "5000\n" +
+            "6000\n" +
+            "\n" +
+            "7000\n" +
+            "8000\n" +
+            "9000\n" +
+            "\n" +
+            "10000"
+    @Test
+    fun exampleTest() {
+        assertEquals(24000,Day202201.part1(input.lines()))
+    }
     @Test
     fun part1Test() {
-        assertEquals(0,useLines(2022,1) { Day202201.part1(it) })
+        assertEquals(69281,useLines(2022,1) { Day202201.part1(it.toList()) })
+    }
+
+    @Test
+    fun exampleTest2() {
+        assertEquals(45000,Day202201.part2(input.lines()))
     }
     @Test
     fun part2Test() {
-        assertEquals(0,useLines(2022,1) { Day202201.part2(it) })
+        assertEquals(201524,useLines(2022,1) { Day202201.part2(it.toList()) })
     }
 }
