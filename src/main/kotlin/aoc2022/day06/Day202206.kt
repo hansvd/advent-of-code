44a743893a9b60b09c30656e1ba74587b981d2c1
Hans van Dodewaard <hans@s104.nl>
2022-12-06 10:46:06 +0100

    2022 - Day 6 refactor

--- src/main/kotlin/aoc2022/day06/Day202206.kt
+++ src/main/kotlin/aoc2022/day06/Day202206.kt
@@ -2,13 +2,12 @@ package aoc2022.day06
 
 object Day202206 {
 
-    fun part1(line: String): Int = findForSize(line.toCharArray())
+    fun part1(line: String): Int = findForSize(line.toList())
 
-    fun part2(line: String): Int = findForSize(line.toCharArray(), 14)
+    fun part2(line: String): Int = findForSize(line.toList(), 14)
 
-    private fun findForSize(input: CharArray, n: Int = 4): Int =
-        (n until input.size).firstOrNull {
-            input.slice(it - n until it)
-                .distinct().size == n
-        } ?: 0
+    private fun findForSize(input: List<Char>, n: Int = 4): Int =
+        input.windowed(n,1).indexOfFirst {
+            it.distinct().size == n
+        } + n
 }
