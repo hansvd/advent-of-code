c9e85f1d82d66a49d9f502e9d419b95cab3d72c5
Hans van Dodewaard <hans@s104.nl>
2022-12-08 11:01:36 +0100

    2022 - Day 8

--- src/main/kotlin/aoc2022/day08/Day202208.kt
+++ src/main/kotlin/aoc2022/day08/Day202208.kt
@@ -2,11 +2,45 @@ package aoc2022.day08
 
 object Day202208 {
 
+    class Grid(private val rows: List<MutableList<Int>>) {
+        val h = rows.size
+        val w = rows[0].size
+
+        private fun isVisible(r: Int, c: Int): Boolean {
+            val i = rows[r][c]
+            return (0 until c).all { rows[r][it] < i }
+                    || (c + 1 until w).all { rows[r][it] < i }
+                    || (0 until r).all { rows[it][c] < i }
+                    || (r + 1 until h).all { rows[it][c] < i }
+        }
+
+        private fun score(r: Int, c: Int): Int {
+            val i = rows[r][c]
+            val s1 = c - ((c - 1 downTo 0).firstOrNull { rows[r][it] >= i } ?: 0)
+            val s2 = ((c + 1 until w).firstOrNull { rows[r][it] >= i } ?: (w - 1)) - c
+            val s3 = r - ((r - 1 downTo 0).firstOrNull { rows[it][c] >= i } ?: 0)
+            val s4 = ((r + 1 until h).firstOrNull { rows[it][c] >= i } ?: (h - 1)) - r
+            return s1 * s2 * s3 * s4
+        }
+
+        fun maxScore(): Int = (1 until h - 1).maxOf { r ->
+            (1 until w - 1).maxOf { c -> score(r, c) }
+        }
+
+        fun countVisible(): Int = (rows.indices).sumOf { r ->
+            (0 until rows[r].size).count { c -> isVisible(r, c) }
+        }
+    }
+
     fun part1(lines: Sequence<String>): Int {
-        return 0
+        return readInput(lines).countVisible()
     }
 
     fun part2(lines: Sequence<String>): Int {
-        return 0
+        return readInput(lines).maxScore()
     }
+
+    private fun readInput(lines: Sequence<String>): Grid = Grid(lines.map { line ->
+        line.map { it.code - '0'.code }.toMutableList()
+    }.toList())
 }
--- src/main/kotlin/aoc2022/day08/Day202208Test.kt
+++ src/main/kotlin/aoc2022/day08/Day202208Test.kt
@@ -6,26 +6,30 @@ import kotlin.test.assertEquals
 
 class Day202208Test {
 
-    val exInput = """"""
+    val exInput = """30373
+25512
+65332
+33549
+35390"""
 
     @Test
     fun part1ExTest() {
-        assertEquals(0, Day202208.part1(exInput.lineSequence()))
+        assertEquals(21, Day202208.part1(exInput.lineSequence()))
     }
 
     @Test
     fun part1Test() {
-        assertEquals(0,useLines(2022,8) { Day202208.part1(it) })
+        assertEquals(1812,useLines(2022,8) { Day202208.part1(it) })
     }
 
 
     @Test
     fun part2ExTest() {
-        assertEquals(0, Day202208.part2(exInput.lineSequence()))
+        assertEquals(8, Day202208.part2(exInput.lineSequence()))
     }
 
     @Test
     fun part2Test() {
-        assertEquals(0,useLines(2022,8) { Day202208.part2(it) })
+        assertEquals(315495,useLines(2022,8) { Day202208.part2(it) })
     }
 }
