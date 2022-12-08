dfa075b41769ebbf9dde6f31ef3473def11e6824
Hans van Dodewaard <hans@s104.nl>
2022-12-08 11:12:01 +0100

    2022 - Day 9 init

---
+++ src/main/kotlin/aoc2022/day09/Day202209.kt
@@ -0,0 +1,12 @@
+package aoc2022.day09
+
+object Day202209 {
+
+    fun part1(lines: Sequence<String>): Int {
+        return 0
+    }
+
+    fun part2(lines: Sequence<String>): Int {
+        return 0
+    }
+}
