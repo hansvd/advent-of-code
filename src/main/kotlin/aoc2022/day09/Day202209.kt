e42d35498a311c53ee55d6e11d185fe04137c6da
Hans van Dodewaard <hans@s104.nl>
2022-12-09 10:26:25 +0100

    2022 - Day 9

--- src/main/kotlin/aoc2022/day09/Day202209.kt
+++ src/main/kotlin/aoc2022/day09/Day202209.kt
@@ -2,11 +2,105 @@ package aoc2022.day09
 
 object Day202209 {
 
-    fun part1(lines: Sequence<String>): Int {
-        return 0
+    data class Point(val x: Int, val y: Int) {
+        val up get() = Point(x, y + 1)
+        val down get() = Point(x, y - 1)
+        val left get() = Point(x - 1, y)
+        val right get() = Point(x + 1, y)
+
+        override fun toString(): String = "($x,$y)"
     }
 
+    class Grid(tailNb: Int = 2) {
+        private var knots = MutableList(tailNb) { Point(0, 0) }
+        private val trail = mutableListOf(Point(0, 0))
+
+        val head get() = knots.first()
+        val tail get() = knots.last()
+        fun handleInput(lines: Sequence<String>): Int {
+            lines.forEach { l ->
+                val c = l[0]
+                val nb = Integer.valueOf(l.substringAfter(" "))
+
+                repeat(nb) {
+                    step(c)
+                }
+            }
+            return trail.toSet().size
+        }
+
+
+        fun step(c: Char) {
+            when (c) {
+                'R' -> knots[0] = knots[0].right
+                'L' -> knots[0] = knots[0].left
+                'U' -> knots[0] = knots[0].up
+                'D' -> knots[0] = knots[0].down
+            }
+
+            for (i in 1 until knots.size) {
+                knots[i] = tailStep(knots[i - 1], knots[i])
+            }
+
+            if (trail.last() != tail)
+                trail.add(tail)
+        }
+
+
+        companion object {
+            private fun tailStep(head: Point, tailIn: Point): Point {
+                var tail = tailIn
+                if (head.x - tail.x > 1) {
+                    tail = tail.right
+                    tail = connectY(head, tail)
+                }
+
+                if (tail.x - head.x > 1) {
+                    tail = tail.left
+                    tail = connectY(head, tail)
+                }
+
+                if (head.y - tail.y > 1) {
+                    tail = tail.up
+                    tail = connectX(head, tail)
+                }
+
+                if (tail.y - head.y > 1) {
+                    tail = tail.down
+                    tail = connectX(head, tail)
+                }
+                return tail
+            }
+
+            private fun connectY(head: Point, tail: Point): Point {
+                if (head.y - tail.y > 0)
+                    return tail.up
+
+                if (tail.y - head.y > 0)
+                    return tail.down
+                return tail
+            }
+
+            private fun connectX(head: Point, tail: Point): Point {
+                if (head.x - tail.x > 0)
+                    return tail.right
+
+                if (tail.x - head.x > 0)
+                    return tail.left
+                return tail
+            }
+        }
+
+    }
+
+    fun part1(lines: Sequence<String>): Int {
+        val grid = Grid()
+        return grid.handleInput(lines)
+    }
+
+
     fun part2(lines: Sequence<String>): Int {
-        return 0
+        val grid = Grid(10)
+        return grid.handleInput(lines)
     }
 }
--- src/main/kotlin/aoc2022/day09/Day202209Test.kt
+++ src/main/kotlin/aoc2022/day09/Day202209Test.kt
@@ -1,31 +1,78 @@
 package aoc2022.day09
 
+import aoc2022.day09.Day202209.Point
 import org.junit.jupiter.api.Test
 import shared.Input.useLines
 import kotlin.test.assertEquals
 
 class Day202209Test {
 
-    val exInput = """"""
+    val exInput = """R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2"""
+
+    @Test
+    fun moveTest() {
+        val grid = Day202209.Grid()
+        grid.step('R')
+        assertEquals(Point(0, 0), grid.tail)
+        assertEquals(Point(1, 0), grid.head)
+
+        grid.step('R')
+        assertEquals(Point(1, 0), grid.tail)
+        assertEquals(Point(2, 0), grid.head)
+
+        repeat(2) { grid.step('R'); }
+        assertEquals(Point(3, 0), grid.tail)
+        assertEquals(Point(4, 0), grid.head)
+
+
+        grid.step('U')
+        assertEquals(Point(3, 0), grid.tail)
+        assertEquals(Point(4, 1), grid.head)
+
+        grid.step('U')
+        assertEquals(Point(4, 1), grid.tail)
+        assertEquals(Point(4, 2), grid.head)
+    }
 
     @Test
     fun part1ExTest() {
-        assertEquals(0, Day202209.part1(exInput.lineSequence()))
+         assertEquals(13, Day202209.part1(exInput.lineSequence()))
     }
 
     @Test
     fun part1Test() {
-        assertEquals(0,useLines(2022,9) { Day202209.part1(it) })
+        assertEquals(6037,useLines(2022,9) { Day202209.part1(it) })
     }
 
 
     @Test
     fun part2ExTest() {
-        assertEquals(0, Day202209.part2(exInput.lineSequence()))
+        assertEquals(1, Day202209.part2(exInput.lineSequence()))
+    }
+
+    @Test
+    fun part2Ex2Test() {
+        val input = """R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20"""
+        assertEquals(36, Day202209.part2(input.lineSequence()))
+
     }
 
     @Test
     fun part2Test() {
-        assertEquals(0,useLines(2022,9) { Day202209.part2(it) })
+        assertEquals(2485,useLines(2022,9) { Day202209.part2(it) })
     }
 }
