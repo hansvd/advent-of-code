2c3b41ad3ec1abb45fa796854e541164eb164349
Hans van Dodewaard <hans@s104.nl>
2022-12-11 12:34:27 +0100

    2022 - Day 11, part 2, refactor

--- src/main/kotlin/aoc2022/day11/Day202211.kt
+++ src/main/kotlin/aoc2022/day11/Day202211.kt
@@ -1,26 +1,37 @@
 package aoc2022.day11
 
-object Day202211 {
+class Day202211(lines: List<String>, val trust: Boolean) {
+    val monkeys: List<Monkey> = lines.chunked(7).map { parse(it) }
+    val scale = monkeys.map { it.testDiv }.fold(1) { acc, i -> acc * i }.toLong()
 
-    class Monkey(
+
+    private fun parse(lines: List<String>): Monkey {
+        val id = Integer.valueOf(lines[0].substringAfter(' ').trimEnd(':'))
+        val items = lines[1].substringAfter("items: ").split(",").map { Integer.valueOf(it.trim()).toLong() }
+        val op = lines[2].substringAfter("new = ").split(' ')
+        val testDiv = Integer.valueOf(lines[3].substringAfterLast(' '))
+        val trueId = Integer.valueOf(lines[4].substringAfterLast(' '))
+        val falseId = Integer.valueOf(lines[5].substringAfterLast(' '))
+        return Monkey(id, items.toMutableList(), op, testDiv, trueId, falseId)
+    }
+
+    inner class Monkey(
         private val id: Int,
         private val items: MutableList<Long>,
         private val operation: List<String>,
-        private val testDiv: Int,
+        val testDiv: Int,
         private val trueId: Int,
         private val falseId: Int
     ) {
         var inspectNb = 0L
 
-        fun inspect(monkeys: List<Monkey>, trust:Boolean) {
-            val scale = monkeys.map { it.testDiv }.fold(1) { acc,i -> acc*i}
+        fun inspect() {
+
             for (item in items.toList()) {
                 inspectNb++
                 var i = calc(item)
                 if (trust) i /= 3 else i %= scale
                 val m = monkeys.firstOrNull { it.id == (if (i % testDiv == 0L) trueId else falseId) } ?: continue
-
-
                 m.items.add(i)
                 items.remove(item)
             }
@@ -29,55 +40,23 @@ object Day202211 {
         private fun calc(item: Long): Long {
             val i1 = if (operation[0] == "old") item else Integer.valueOf(operation[0]).toLong()
             val i2 = if (operation[2] == "old") item else Integer.valueOf(operation[2]).toLong()
-            if (operation[1] == "+") return i1+i2
-            else return i1*i2
-        }
-
-        companion object {
-            fun parse(lines: List<String>): Monkey {
-                val id = Integer.valueOf(lines[0].substringAfter(' ').trimEnd(':'))
-                val items = lines[1].substringAfter("items: ").split(",").map { Integer.valueOf(it.trim()).toLong() }
-                val op = lines[2].substringAfter("new = ").split(' ')
-                val testDiv = Integer.valueOf(lines[3].substringAfterLast(' '))
-                val trueId = Integer.valueOf(lines[4].substringAfterLast(' '))
-                val falseId = Integer.valueOf(lines[5].substringAfterLast(' '))
-                return Monkey(id, items.toMutableList(), op, testDiv, trueId, falseId)
-            }
+            return if (operation[1] == "+") i1 + i2 else i1 * i2
         }
     }
 
-    fun part1(lines: List<String>): Long {
-        val monkeys = parseInput(lines)
-        repeat(20) {
+    private fun invoke(count: Int): Long {
+        repeat(count) {
             for (m in monkeys) {
-                m.inspect(monkeys,true)
+                m.inspect()
             }
         }
         val r = monkeys.map { it.inspectNb }.sortedDescending().take(2)
         return r[0] * r[1]
     }
 
-    fun part2(lines: List<String>): Long {
-        val monkeys = parseInput(lines)
-        for (i in 0 until 10_000) {
-            for (m in monkeys) {
-                m.inspect(monkeys,false)
-            }
-        }
-        val r = monkeys.map { it.inspectNb }.sortedDescending().take(2)
-        return r[0] * r[1]
-    }
 
-    fun parseInput(lines: List<String>): List<Monkey> {
-        var monkeyStart = -1
-        return sequence {
-            lines.forEachIndexed { index, s ->
-                if (s.startsWith("Monkey")) {
-                    if (monkeyStart >= 0) yield(Monkey.parse(lines.subList(monkeyStart, index - 1)))
-                    monkeyStart = index
-                }
-            }
-            yield(Monkey.parse(lines.subList(monkeyStart, lines.size)))
-        }.toList()
+    companion object {
+        fun part1(lines: List<String>): Long = Day202211(lines, true).invoke(20)
+        fun part2(lines: List<String>): Long = Day202211(lines, false).invoke(10_000)
     }
 }
