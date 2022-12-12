00b970a9aa7ac80b5f1a7e51d38bbd7246db95da
Hans van Dodewaard <hans@s104.nl>
2022-12-12 15:42:35 +0100

    2022 - Day 13,init

---
+++ src/main/kotlin/aoc2022/day13/Day202213.kt
@@ -0,0 +1,12 @@
+package aoc2022.day13
+
+object Day202213 {
+
+    fun part1(lines: Sequence<String>): Int {
+        return 0
+    }
+
+    fun part2(lines: Sequence<String>): Int {
+        return 0
+    }
+}
---
+++ src/main/kotlin/aoc2022/day13/Day202213Test.kt
@@ -0,0 +1,31 @@
+package aoc2022.day13
+
+import org.junit.jupiter.api.Test
+import shared.Input.useLines
+import kotlin.test.assertEquals
+
+class Day202213Test {
+
+    val exInput = """"""
+
+    @Test
+    fun part1ExTest() {
+        assertEquals(0, Day202213.part1(exInput.lineSequence()))
+    }
+
+    @Test
+    fun part1Test() {
+        assertEquals(0,useLines(2022,13) { Day202213.part1(it) })
+    }
+
+
+    @Test
+    fun part2ExTest() {
+        assertEquals(0, Day202213.part2(exInput.lineSequence()))
+    }
+
+    @Test
+    fun part2Test() {
+        assertEquals(0,useLines(2022,13) { Day202213.part2(it) })
+    }
+}
