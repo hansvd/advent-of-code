85daa1271518bf74daa1a4322bbc3b9812d3e726
Hans van Dodewaard <hans@s104.nl>
2022-12-13 14:41:37 +0100

    2022 - Day 14 init

---
+++ src/main/kotlin/aoc2022/day14/Day202214.kt
@@ -0,0 +1,12 @@
+package aoc2022.day14
+
+object Day202214 {
+
+    fun part1(lines: Sequence<String>): Int {
+        return 0
+    }
+
+    fun part2(lines: Sequence<String>): Int {
+        return 0
+    }
+}
--- src/main/kotlin/aoc2022/day14/Day202214Test.kt
+++ src/main/kotlin/aoc2022/day14/Day202214Test.kt
@@ -1,11 +1,10 @@
-package aoc2022.day25
+package aoc2022.day14
 
-import aoc2022.day14.Day202214
 import org.junit.jupiter.api.Test
 import shared.Input.useLines
 import kotlin.test.assertEquals
 
-class Day202225Test {
+class Day202214Test {
 
     val exInput = """"""
 
@@ -16,7 +15,7 @@ class Day202225Test {
 
     @Test
     fun part1Test() {
-        assertEquals(0,useLines(2022,25) { Day202214.part1(it) })
+        assertEquals(0,useLines(2022,14) { Day202214.part1(it) })
     }
 
 
@@ -27,6 +26,6 @@ class Day202225Test {
 
     @Test
     fun part2Test() {
-        assertEquals(0,useLines(2022,25) { Day202214.part2(it) })
+        assertEquals(0,useLines(2022,14) { Day202214.part2(it) })
     }
 }
