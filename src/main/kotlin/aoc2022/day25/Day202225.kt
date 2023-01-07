bf00da3e8581b81af074b97b465c82cc3fbc4555
Hans van Dodewaard <hans@s104.nl>
2023-01-07 13:40:57 +0100

    2022 - Day 25

--- src/main/kotlin/aoc2022/day25/Day202225.kt
+++ src/main/kotlin/aoc2022/day25/Day202225.kt
@@ -1,12 +1,52 @@
 package aoc2022.day25
+import kotlin.math.pow
 
 object Day202225 {
 
-    fun part1(lines: Sequence<String>): Int {
-        return 0
-    }
+    fun part1(lines: Sequence<String>): String = decToSnafu(lines.map { snafuToDec(it)}.sum())
 
-    fun part2(lines: Sequence<String>): Int {
-        return 0
+    private fun Int.pow(exp:Int) = this.toDouble().pow(exp).toLong()
+
+    fun snafuToDec(snafu: String): Long =
+        snafu.reversed().foldIndexed(0L) { index, r, el  ->
+            when (el) {
+                '1' -> r + 1 * 5.pow(index)
+                '2' -> r+ 2 * 5.pow(index)
+                '-' -> r - 1 * 5.pow(index)
+                '=' -> r - 2 * 5.pow(index)
+                else -> r
+            }
+        }
+    fun decToSnafu(dec: Long): String {
+        val result = mutableListOf<Long>()
+        var d = dec
+        do  {
+            val c = d % 5
+            result.add(c)
+            d /= 5
+        } while (d > 0)
+        result.add(0)
+
+        (0 until result.size).forEach {i ->
+            if (result[i] > 4) {
+                result[i] -= 5L
+                result[i+1] += 1L
+            }
+            when {
+                result[i] == 3L -> {
+                    result[i] = -2
+                    result[i+1] += 1L
+                }
+                result[i] == 4L -> {
+                    result[i] = -1
+                    result[i+1] += 1L
+                }
+            }
+        }
+        return result.reversed().fold("") { a, b -> a + when(b) {
+            -2L -> "="
+            -1L -> "-"
+            else -> b
+        } }.trimStart('0')
     }
 }
--- src/main/kotlin/aoc2022/day25/Day202225Test.kt
+++ src/main/kotlin/aoc2022/day25/Day202225Test.kt
@@ -6,26 +6,56 @@ import kotlin.test.assertEquals
 
 class Day202225Test {
 
-    val exInput = """"""
+    private val exInput = """1=-0-2
+12111
+2=0=
+21
+2=01
+111
+20012
+112
+1=-1=
+1-12
+12
+1=
+122"""
+
+    private val testValues = listOf(
+        1 to "1",
+        2 to "2",
+        3 to "1=",
+        4 to "1-",
+        5 to "10",
+        6 to "11",
+        7 to "12",
+        8 to "2=",
+        9 to "2-",
+        10 to "20",
+        15 to "1=0",
+        20 to "1-0",
+        2022 to "1=11-2",
+        12345 to "1-0---0",
+        314159265 to "1121-1110-1=0"
+    )
+
+    @Test
+    fun convertTest() {
+        testValues.forEach {
+            assertEquals(it.first.toLong(), Day202225.snafuToDec(it.second))
+        }
+        testValues.forEach {
+            assertEquals(it.second, Day202225.decToSnafu(it.first.toLong()))
+        }
+    }
 
     @Test
     fun part1ExTest() {
-        assertEquals(0, Day202225.part1(exInput.lineSequence()))
+        assertEquals("2=-1=0", Day202225.part1(exInput.lineSequence()))
     }
 
     @Test
     fun part1Test() {
-        assertEquals(0,useLines(2022,25) { Day202225.part1(it) })
+        assertEquals("2-1-110-=01-1-0-0==2", useLines(2022, 25) { Day202225.part1(it) })
     }
 
-
-    @Test
-    fun part2ExTest() {
-        assertEquals(0, Day202225.part2(exInput.lineSequence()))
-    }
-
-    @Test
-    fun part2Test() {
-        assertEquals(0,useLines(2022,25) { Day202225.part2(it) })
-    }
 }
