48047a086f28427e2595c3353bd9571fda9f0df5
Hans van Dodewaard <hans@s104.nl>
2023-12-02 09:31:52 +0100

    2023, day 2, part 1

--- src/main/kotlin/aoc2023/day02/Day.kt
+++ src/main/kotlin/aoc2023/day02/Day.kt
@@ -1,8 +1,40 @@
 package aoc2023.day02
-
+typealias SamleSet =List<Day.Sample>
 object Day {
 
-    fun part1(lines: List<String>): Int = 0
+    data class Sample(val number:Int, val color:String)
+
+    data class Game(val id:Int, val samples:List<SamleSet>)
+    //only 12 red cubes, 13 green cubes, and 14 blue cubes
+    fun part1(lines: List<String>):Int {
+        val games = parseInput(lines)
+        return games.mapNotNull { game ->
+            if (game.samples.all { set ->
+                    set.all {
+                        when (it.color) {
+                            "red" -> it.number <= 12
+                            "green" -> it.number <= 13
+                            "blue" -> it.number <= 14
+                            else -> false
+                        }
+                    }
+                }
+            ) game.id else null
+        }.sum()
+    }
+
 
     fun part2(lines: List<String>): Int = 0
+
+    private fun parseInput(lines: List<String>):List<Game> = lines.map { line ->
+        val game = line.split(":")
+        Game(game[0].split(' ')[1].toInt(),
+            game[1].split(';').map { set ->
+            set.split(',').map { sample ->
+                val s = sample.trim().split(' ')
+                Sample(s[0].toInt(), s[1])
+            }
+        })
+    }
+
 }
--- src/main/kotlin/aoc2023/day02/DayTest.kt
+++ src/main/kotlin/aoc2023/day02/DayTest.kt
@@ -9,14 +9,19 @@ class DayTest {
     private val year = 2023
     private val day = 2
 
-    private val input = ""
+    private val input = """Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"""
+
     @Test
     fun exampleTest() {
-        assertEquals(0,Day.part1(input.lines()))
+        assertEquals(8,Day.part1(input.lines()))
     }
     @Test
     fun part1Test() {
-        assertEquals(0,useLines(year,day) { Day.part1(it.toList()) })
+        assertEquals(2447,useLines(year,day) { Day.part1(it.toList()) })
     }
 
     @Test
