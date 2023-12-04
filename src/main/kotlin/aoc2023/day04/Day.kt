0d045d6ade65608d69825d84d48904ae5e37d4f7
Hans van Dodewaard <hans@s104.nl>
2023-12-04 09:50:00 +0100

    2023, day 4, part 1

--- src/main/kotlin/aoc2023/day04/Day.kt
+++ src/main/kotlin/aoc2023/day04/Day.kt
@@ -1,8 +1,21 @@
 package aoc2023.day04
 
-object Day {
+import kotlin.math.pow
 
-    fun part1(lines: List<String>): Int = 0
+object Day {
+    private fun Int.pow(exp:Int) = this.toDouble().pow(exp).toLong()
+
+    data class Card(val winning:List<Int>, val our:List<Int>)
+    fun part1(lines: List<String>) =
+        parseInput(lines).map { card ->
+            card.our.filter { card.winning.contains(it) }.size
+        }.sumOf { if (it == 0) 0 else 2.pow(it - 1) }
 
     fun part2(lines: List<String>): Int = 0
+
+    fun parseInput(lines: List<String>):List<Card> = lines.map { line ->
+        val parts = line.split(":")[1].split('|')
+        Card(parts[0].trim().split(' ').filter { it.isNotBlank() }.map { it.toInt() },
+            parts[1].trim().split(' ').filter { it.isNotBlank() }.map { it.toInt() })
+    }
 }
--- src/main/kotlin/aoc2023/day04/DayTest.kt
+++ src/main/kotlin/aoc2023/day04/DayTest.kt
@@ -9,19 +9,24 @@ class DayTest {
     private val year = 2023
     private val day = 4
 
-    private val input = ""
+    private val input = """Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"""
     @Test
     fun exampleTest() {
-        assertEquals(0,Day.part1(input.lines()))
+        assertEquals(13,Day.part1(input.lines()))
     }
     @Test
     fun part1Test() {
-        assertEquals(0,useLines(year,day) { Day.part1(it.toList()) })
+        assertEquals(25004,useLines(year,day) { Day.part1(it.toList()) })
     }
 
     @Test
     fun exampleTest2() {
-        assertEquals(0,Day.part2(input.lines()))
+        assertEquals(25004,Day.part2(input.lines()))
     }
     @Test
     fun part2Test() {
