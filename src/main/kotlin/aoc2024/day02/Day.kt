417398d24ff6da2f493d1b8e2c8283ef6a990747
Hans van Dodewaard <hans@s104.nl>
2024-12-04 21:41:30 +0100

    2024, day 2

--- src/main/kotlin/aoc2024/day02/Day.kt
+++ src/main/kotlin/aoc2024/day02/Day.kt
@@ -4,32 +4,17 @@ import kotlin.math.absoluteValue
 
 object Day {
 
-    fun part1(lines: List<String>): Int {
-        val input = parseInput(lines)
-        return input.count { isSave(it) }
-    }
-    fun part2(lines: List<String>): Int {
-        val input = parseInput(lines)
-        return input.count { isSave(it, true) }
-    }
-    private fun isSave(levels: List<Int>, mayRemoveLevel: Boolean=false): Boolean {
+    fun part1(lines: List<String>): Int = parseInput(lines).count { isSave(it) }
+    fun part2(lines: List<String>): Int = parseInput(lines).count { isSavePart2(it) }
 
-        val increaseCount = levels.indices.count { i -> i > 0 && levels[i] > levels[i-1] }
-        val decreaseCount = levels.indices.count { i -> i > 0 && levels[i] < levels[i-1] }
-        val diffCount = levels.indices.count { i -> i > 0 && (levels[i] - levels[i-1]).absoluteValue in 1..3 }
-        if ((increaseCount == levels.size - 1 || decreaseCount == levels.size - 1) && diffCount == levels.size - 1) {
-            return true
-        }
-        if (!mayRemoveLevel) return false
-        levels.indices.forEach { i ->
-            if (isSave(levels.filterIndexed { index, _ -> index != i }, false)) {
-                return true
-            }
-        }
-        return false
+    private fun isSavePart2(levels: List<Int>): Boolean =
+        isSave(levels) || levels.indices.any { i -> isSave(levels.filterIndexed { index, _ -> index != i }) }
+
+    private fun isSave(levels: List<Int>): Boolean = levels.zipWithNext().let { zipped ->
+        (zipped.count { (prev, current) -> (prev - current).absoluteValue in 1..3 } == zipped.size) &&
+         ((zipped.count { (prev, current) -> current > prev } == zipped.size) ||
+          (zipped.count { (prev, current) -> current < prev } == zipped.size))
     }
-
-
 
     fun parseInput(lines: List<String>): List<List<Int>> = lines.map {
         it.split(" ").map { it.toInt() }
