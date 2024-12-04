67b8ac20dcd1ffb5b1756ad01f1e3f6af6c45eb4
Hans van Dodewaard <hans@s104.nl>
2024-12-04 21:06:18 +0100

    2024, day 2

--- src/main/kotlin/aoc2024/day02/Day.kt
+++ src/main/kotlin/aoc2024/day02/Day.kt
@@ -1,10 +1,37 @@
 package aoc2024.day02
 
+import kotlin.math.absoluteValue
+
 object Day {
 
-    fun part1(lines: List<String>): Int = 0
+    fun part1(lines: List<String>): Int {
+        val input = parseInput(lines)
+        return input.count { isSave(it) }
+    }
+    fun part2(lines: List<String>): Int {
+        val input = parseInput(lines)
+        return input.count { isSave(it, true) }
+    }
+    private fun isSave(levels: List<Int>, mayRemoveLevel: Boolean=false): Boolean {
 
-    fun part2(lines: List<String>): Int = 0
+        val increaseCount = levels.indices.count { i -> i > 0 && levels[i] > levels[i-1] }
+        val decreaseCount = levels.indices.count { i -> i > 0 && levels[i] < levels[i-1] }
+        val diffCount = levels.indices.count { i -> i > 0 && (levels[i] - levels[i-1]).absoluteValue in 1..3 }
+        if ((increaseCount == levels.size - 1 || decreaseCount == levels.size - 1) && diffCount == levels.size - 1) {
+            return true
+        }
+        if (!mayRemoveLevel) return false
+        levels.indices.forEach { i ->
+            if (isSave(levels.filterIndexed { index, _ -> index != i }, false)) {
+                return true
+            }
+        }
+        return false
+    }
 
-    fun parseInput(lines: List<String>): List<String> = lines
+
+
+    fun parseInput(lines: List<String>): List<List<Int>> = lines.map {
+        it.split(" ").map { it.toInt() }
+    }
 }
--- src/main/kotlin/aoc2024/day02/DayTest.kt
+++ src/main/kotlin/aoc2024/day02/DayTest.kt
@@ -9,22 +9,29 @@ class DayTest {
     private val year = 2024
     private val day = 2
 
-    private val input = """"""
+    private val input = """
+        7 6 4 2 1
+        1 2 7 8 9
+        9 7 6 2 1
+        1 3 2 4 5
+        8 6 4 4 1
+        1 3 6 7 9
+    """.trimIndent()
     @Test
     fun exampleTest() {
-        assertEquals(0,Day.part1(input.lines()))
+        assertEquals(2,Day.part1(input.lines()))
     }
     @Test
     fun part1Test() {
-        assertEquals(0,useLines(year,day) { Day.part1(it.toList()) })
+        assertEquals(402,useLines(year,day) { Day.part1(it.toList()) })
     }
 
     @Test
     fun exampleTest2() {
-        assertEquals(0,Day.part2(input.lines()))
+        assertEquals(4,Day.part2(input.lines()))
     }
     @Test
     fun part2Test() {
-        assertEquals(0,useLines(year,day) { Day.part2(it.toList()) })
+        assertEquals(455,useLines(year,day) { Day.part2(it.toList()) })
     }
 }
